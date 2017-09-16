ec29ae7b6fc0a7ba86286fc65b796c6e20df010c
gurankas <gurankas@gmail.com>
2017-09-16 15:34:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,17 +90,17 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function(req,res)
 {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articles.articleOne));
 });
 
 app.get('/article-two', function(req,res)
 {
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
+  res.send(createTemplate(articles.articleOne));
 });
 
 app.get('/article-three', function(req,res)
 {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+   res.send(createTemplate(articles.articleOne));
 });
 
 app.get('/ui/style.css', function (req, res) {
