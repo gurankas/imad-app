8a1ad39eedcfb6bbf5b8e014ae83e496f00e6d2d
gurankas <gurankas@gmail.com>
2017-09-16 16:42:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
+
+//Change the text after the page is loaded
 var element = document.getElementById('main-content');
 element.innerHTML = '<body><p>This is the text which was changed via javascript</p></body>';
+
+//Move the image after it's loaded
+var image = document.getElementById('logo');
+image.onclick = function(){
+    image.style.leftMargin = '100px';  
+};
