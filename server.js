341fcd486266089c26110bdf046dd408fdc0fc31
gurankas <gurankas@gmail.com>
2017-09-16 15:44:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,17 +91,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function(req,res)
 {
     var articleName = req.params.articleName;   
-   res.send(createTemplate(articles.articleOne));
-});
-
-app.get('/article-two', function(req,res)
-{
-  res.send(createTemplate(articles.articleOne));
-});
-
-app.get('/article-three', function(req,res)
-{
-   res.send(createTemplate(articles.articleOne));
+   res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
